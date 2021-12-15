657d405c2996b4f9b85992d007e72477cb6e3454
Hoang Cong Binh <coby.hoang@finos.asia>
2021-12-15 23:44:39 +0700

    Fix styling issue

--- container/src/App.js
+++ container/src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ct-',
+});
 
 export default () => {
-  return <Router>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
-    </Router>
+  return <div>
+    <StylesProvider generateClassName={generateClassName}>
+      <Router>
+            <div>
+              <Header />
+              <MarketingApp />
+            </div>
+      </Router>
+    </StylesProvider>
+  </div>
 };
