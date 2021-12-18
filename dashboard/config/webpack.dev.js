0cb8e58ef2784df371549d871532417b313fe50e
Hoang Cong Binh <coby.hoang@finos.asia>
2021-12-18 12:03:28 +0700

    Add dashboard app

--- dashboard/config/webpack.dev.js
+++ dashboard/config/webpack.dev.js
@@ -6,31 +6,31 @@ const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8083/',
+  },
   devServer: {
     port: 8083,
     historyApiFallback: {
       index: 'index.html',
     },
     headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  },
-  output: {
-    publicPath: 'http://localhost:8083/'
+      'Access-Control-Allow-Origin': '*',
+    },
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'dashboard',
       filename: 'remoteEntry.js',
       exposes: {
-        './DashboardApp': './src/bootstrap'
+        './DashboardApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
+      template: './public/index.html',
+    }),
+  ],
 };
 
 module.exports = merge(commonConfig, devConfig);
--- dashboard/config/webpack.prod.js
+++ dashboard/config/webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
-const commonConfig = require('../config/webpack.common');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
   mode: 'production',
@@ -14,11 +14,11 @@ const prodConfig = {
       name: 'dashboard',
       filename: 'remoteEntry.js',
       exposes: {
-        './DashboardApp': './src/bootstrap'
+        './DashboardApp': './src/bootstrap',
       },
-      shared: packageJson.dependencies
+      shared: packageJson.dependencies,
     }),
-  ]
+  ],
 };
 
 module.exports = merge(commonConfig, prodConfig);
